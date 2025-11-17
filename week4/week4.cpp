beffb93082cb811b559438e2a592334c6ff5711e
arturbarcij <arturbarcij1@gmail.com>
2025-11-17 21:15:24 +0100

    week 4

---
+++ week4/week4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespacee std;
+
+int main() {
+    vector<int> numers = {1, 2, 3, 4, 5};
+    int sum = 0;
+    for (int n : numbers) {
+        sum += n;
+    }
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
