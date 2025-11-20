a2d2902238020a99c7f056d525e6854a5a2b27b4
arturbarcij <arturbarcij1@gmail.com>
2025-11-20 23:12:09 +0100

    collatz sequence

---
+++ algorithms_and_dsa/weird_algorithm/weird_algorithm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    if (!(cin >> n) || n <= 0) {
+        return 1;
+    }
+
+    cout << n << " ";
+    while (n > 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n = n * 3 + 1; 
+        }
+        cout << n << " ";
+    }
+}
